5f4fb5daf0dcb643fbdbb82f664043d8c42801b1
Dubem Umeh <raphaelumeh21@gmail.com>
2026-08-27 22:07:57 +0000

    Add proxy function to handle unpaid requests

---
+++ proxy.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const isPaid = false;
+
+export function proxy(request: NextRequest) {
+  if (!isPaid) {
+    return NextResponse.redirect(
+      "https://www.google.com/search?q=dubem+umeh&oq=dubem+umeh&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMgkIAhAAGA0YgAQyCQgDEAAYDRiABDIJCAQQABgNGIAEMgYIBRBFGDwyBggGEEUYPDIGCAcQRRg8MgoICBAAGIAEGKIEMgcICRAAGO8FMgoIChAAGIAEGKIEMgoICxAAGIAEGKIEMgcIDBAAGO8FMgcIDRAhGI8CMgcIDhAhGI8C0gEIMzgwM2owajSoAgKwAgE&client=ms-android-xiaomi-rvo3&sourceid=chrome-mobile&source=chrome.ob&ie=UTF-8"
+    );
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|favicon_io).*)"],
+};
