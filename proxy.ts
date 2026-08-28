c71e89ba7fd4548de0b2cb89121ca9fd183c24e1
Dubem Umeh <raphaelumeh21@gmail.com>
2026-08-28 14:24:32 +0000

    Change isPaid status from false to true

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const isPaid = false;
+const isPaid = true;
 
 export function proxy(request: NextRequest) {
   if (!isPaid) {
