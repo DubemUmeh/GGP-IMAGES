33ec1b1cbfeef083f75433a64f390f97c97b23a7
Dubem Umeh <raphaelumeh21@gmail.com>
2026-09-05 01:09:20 +0000

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const isPaid = true;
+const isPaid = false;
 
 export function proxy(request: NextRequest) {
   if (!isPaid) {
